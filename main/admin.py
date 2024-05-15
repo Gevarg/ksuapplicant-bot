c7ff4e1ebe5e89fef18f470c45485e2bfe7ed5ae
Gevarg <37332468+Gevarg@users.noreply.github.com>
2024-05-15 04:39:30 +0300

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Category, Tag, UserQueryLog, Answer
 
+
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
 admin.site.register(Category)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(UserQueryLog)
 admin.site.register(Answer)
