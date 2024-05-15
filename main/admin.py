7bd44bb13d70b449818302e651c9f322bef9be8e
Gevarg <37332468+Gevarg@users.noreply.github.com>
2024-05-15 07:51:31 +0300

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Tag, UserQueryLog, Answer
+from .models import Category, Tag, UserQueryLog, Answer, FAQ
 
 
 class TagAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ admin.site.register(Category)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(UserQueryLog)
 admin.site.register(Answer)
+admin.site.register(FAQ)
